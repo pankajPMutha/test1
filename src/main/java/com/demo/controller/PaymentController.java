02d164cc3a28581433b7c3ff7478231489ffb3f0
ppankajlinkedinppankaj <ppankajplinkedin@gmail.com>
2024-04-28 11:25:41 +0530

    2nd commit

---
+++ src/main/java/com/demo/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.demo.controller;
+
+public class PaymentController {
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
